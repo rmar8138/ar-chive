8ffd3135831c3cd360132ea327c56ff33c7ed209
rmar8138 <ragan.martinez@live.com>
2019-09-11 21:29:33 +1000

    Add gatsby-image to micra project page and fix cool mac cafe images

--- src/pages/micra.js
+++ src/pages/micra.js
@@ -1,23 +1,11 @@
 import React, { Fragment } from "react"
-import styled from "styled-components"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import ProjectPageLayout from "../components/projectPageLayout"
 import MobileProjectPageLayout from "../components/mobileProjectPageLayout"
 import { isBrowser } from "react-device-detect"
 
-const img2 = require(`../assets/images/web/micra/2.png`)
-const img3 = require(`../assets/images/web/micra/3.png`)
-const img4 = require(`../assets/images/web/micra/4.png`)
-const img5 = require(`../assets/images/web/micra/5.png`)
-const img6 = require(`../assets/images/web/micra/6.png`)
-const img7 = require(`../assets/images/web/micra/7.png`)
-
-const img1_1 = require(`../assets/images/web/micra/1-1.png`)
-const img1_2 = require(`../assets/images/web/micra/1-2.png`)
-const img6_1 = require(`../assets/images/web/micra/6-1.png`)
-const img6_2 = require(`../assets/images/web/micra/6-2.png`)
-const img6_3 = require(`../assets/images/web/micra/6-3.png`)
-
-const Micra = () =>
+const Micra = ({ data }) =>
   isBrowser ? (
     <ProjectPageLayout heading="05/Micra">
       <Fragment>
@@ -29,22 +17,22 @@ const Micra = () =>
         </p>
       </Fragment>
       <Fragment>
-        <img
-          src={img2}
+        <Img
+          fluid={data.allFile.edges[4].node.childImageSharp.fluid}
           alt="Micra"
           style={{ width: "50%", minWidth: "30rem", maxWidth: "50rem" }}
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img3}
+        <Img
+          fluid={data.allFile.edges[0].node.childImageSharp.fluid}
           alt="Micra"
           style={{ width: "40%", minWidth: "30rem", maxWidth: "40rem" }}
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img4}
+        <Img
+          fluid={data.allFile.edges[3].node.childImageSharp.fluid}
           alt="Micra"
           style={{ width: "60%", minWidth: "40rem", maxWidth: "60rem" }}
         />
@@ -56,22 +44,22 @@ const Micra = () =>
         </p>
       </Fragment>
       <Fragment>
-        <img
-          src={img5}
+        <Img
+          fluid={data.allFile.edges[2].node.childImageSharp.fluid}
           alt="Micra"
           style={{ width: "60%", minWidth: "40rem", maxWidth: "60rem" }}
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img6}
+        <Img
+          fluid={data.allFile.edges[6].node.childImageSharp.fluid}
           alt="Micra"
           style={{ width: "60%", minWidth: "40rem", maxWidth: "60rem" }}
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img7}
+        <Img
+          fluid={data.allFile.edges[5].node.childImageSharp.fluid}
           alt="Micra"
           style={{ width: "80%", minWidth: "40rem", maxWidth: "80rem" }}
         />
@@ -88,26 +76,30 @@ const Micra = () =>
         </p>
       </Fragment>
       <Fragment>
-        <img
-          src={img1_1}
+        <Img
+          fluid={data.allFile.edges[10].node.childImageSharp.fluid}
           alt="Micra"
-          style={{ width: "45%", marginTop: "-15rem" }}
+          style={{ width: "45%", marginTop: "-15rem", marginRight: "1rem" }}
         />
-        <img
-          src={img1_2}
+        <Img
+          fluid={data.allFile.edges[11].node.childImageSharp.fluid}
           alt="Micra"
-          style={{ width: "45%", marginTop: "15rem" }}
+          style={{ width: "45%", marginTop: "15rem", marginLeft: "1rem" }}
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img2}
+        <Img
+          fluid={data.allFile.edges[0].node.childImageSharp.fluid}
           alt="Micra"
           style={{ width: "90%", marginRight: "auto" }}
         />
       </Fragment>
       <Fragment>
-        <img src={img3} alt="Micra" style={{ width: "100%" }} />
+        <Img
+          fluid={data.allFile.edges[3].node.childImageSharp.fluid}
+          alt="Micra"
+          style={{ width: "100%" }}
+        />
       </Fragment>
       <Fragment>
         <p>
@@ -116,26 +108,34 @@ const Micra = () =>
         </p>
       </Fragment>
       <Fragment>
-        <img src={img4} alt="Micra" style={{ width: "100%" }} />
-      </Fragment>
-      <Fragment>
-        <img src={img5} alt="Micra" style={{ width: "100%" }} />
-      </Fragment>
-      <Fragment>
-        <img
-          src={img6_1}
+        <Img
+          fluid={data.allFile.edges[2].node.childImageSharp.fluid}
           alt="Micra"
-          style={{ width: "45%", marginTop: "-15rem" }}
-        />
-        <img
-          src={img6_2}
-          alt="Micra"
-          style={{ width: "45%", marginTop: "15rem" }}
+          style={{ width: "100%" }}
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img6_3}
+        <Img
+          fluid={data.allFile.edges[6].node.childImageSharp.fluid}
+          alt="Micra"
+          style={{ width: "100%" }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[9].node.childImageSharp.fluid}
+          alt="Micra"
+          style={{ width: "45%", marginTop: "-15rem", marginRight: "1rem" }}
+        />
+        <Img
+          fluid={data.allFile.edges[8].node.childImageSharp.fluid}
+          alt="Micra"
+          style={{ width: "45%", marginTop: "15rem", marginLeft: "1rem" }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[7].node.childImageSharp.fluid}
           alt="Micra"
           style={{ width: "90%", marginRight: "auto" }}
         />
@@ -144,3 +144,20 @@ const Micra = () =>
   )
 
 export default Micra
+
+export const query = graphql`
+  query {
+    allFile(filter: { relativeDirectory: { eq: "web/micra" } }) {
+      edges {
+        node {
+          id
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
