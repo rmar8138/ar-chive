9b31f870acfaa987dc30fff95735cae02bb6e74e
rmar8138 <ragan.martinez@live.com>
2019-09-13 15:58:47 +1000

    Add gastby-image to other worlds project page

--- src/pages/other-worlds.js
+++ src/pages/other-worlds.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import ProjectPageLayout from "../components/projectPageLayout"
 import MobileProjectPageLayout from "../components/mobileProjectPageLayout"
 import { isBrowser } from "react-device-detect"
@@ -15,12 +17,12 @@ const img9 = require("../assets/images/web/other-worlds/9.png")
 const img10 = require("../assets/images/web/other-worlds/10.png")
 const img11 = require("../assets/images/web/other-worlds/11.png")
 
-const OtherWorlds = () =>
+const OtherWorlds = ({ data }) =>
   isBrowser ? (
     <ProjectPageLayout heading="10/Other Worlds">
       <Fragment>
-        <img
-          src={img1}
+        <Img
+          fluid={data.allFile.edges[4].node.childImageSharp.fluid}
           alt="Other Worlds Front Cover"
           style={{
             marginLeft: "40%",
@@ -39,22 +41,9 @@ const OtherWorlds = () =>
           paintings for each track of the album.
         </p>
       </Fragment>
-      {/* {art.map(song => (
-        <Fragment>
-          <img
-            src={song.img}
-            alt={song.alt}
-            style={{
-              width: "70%",
-              minWidth: "40rem",
-              maxWidth: "80rem",
-            }}
-          />
-        </Fragment>
-      ))} */}
       <Fragment>
-        <img
-          src={img2}
+        <Img
+          fluid={data.allFile.edges[9].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -64,8 +53,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img3}
+        <Img
+          fluid={data.allFile.edges[6].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -75,8 +64,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img4}
+        <Img
+          fluid={data.allFile.edges[2].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -86,8 +75,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img5}
+        <Img
+          fluid={data.allFile.edges[10].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -97,8 +86,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img6}
+        <Img
+          fluid={data.allFile.edges[0].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -108,8 +97,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img7}
+        <Img
+          fluid={data.allFile.edges[8].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -119,8 +108,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img8}
+        <Img
+          fluid={data.allFile.edges[3].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -130,8 +119,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img9}
+        <Img
+          fluid={data.allFile.edges[7].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -141,8 +130,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img10}
+        <Img
+          fluid={data.allFile.edges[5].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -152,8 +141,8 @@ const OtherWorlds = () =>
         />
       </Fragment>
       <Fragment>
-        <img
-          src={img11}
+        <Img
+          fluid={data.allFile.edges[1].node.childImageSharp.fluid}
           alt=""
           style={{
             width: "70%",
@@ -164,7 +153,133 @@ const OtherWorlds = () =>
       </Fragment>
     </ProjectPageLayout>
   ) : (
-    <MobileProjectPageLayout heading="10/Other Worlds" />
+    <MobileProjectPageLayout heading="10/Other Worlds">
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[4].node.childImageSharp.fluid}
+          alt="Other Worlds Front Cover"
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <p>
+          ‘Other Worlds’ is the debut album by Ragan Martinez. He uses ambient
+          and distorted sounds on loop, creating an ominous yet carthatic
+          meditative release. It is the soundtrack to a video game that doesn’t
+          exist. We art directed and designed the cover as well as individual
+          paintings for each track of the album.
+        </p>
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[9].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[6].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[2].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[10].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[0].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[8].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[3].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[7].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[5].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+      <Fragment>
+        <Img
+          fluid={data.allFile.edges[1].node.childImageSharp.fluid}
+          alt=""
+          style={{
+            width: "100%",
+          }}
+        />
+      </Fragment>
+    </MobileProjectPageLayout>
   )
 
 export default OtherWorlds
+
+export const query = graphql`
+  query {
+    allFile(filter: { relativeDirectory: { eq: "web/other-worlds" } }) {
+      edges {
+        node {
+          id
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
