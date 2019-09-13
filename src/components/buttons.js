550ad1bc05a4e5db292e009028f24384ca88fe9f
rmar8138 <ragan.martinez@live.com>
2019-09-13 15:17:32 +1000

    Add gatsby-image to mobile projects page and add feature that allows users to move to project page upon clicking image

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -11,9 +11,11 @@ export const BackButton = styled(Link)`
   z-index: 10000000;
 `
 export const AllProjectsLink = styled(Link)`
+  font-family: "Grotesk Regular";
   text-transform: uppercase;
-  font-size: 1.6rem;
-
-  align-self: flex-start;
-  margin-top: 1rem;
+  font-size: ${props => props.theme.fs_small};
+  position: absolute;
+  bottom: 2rem;
+  right: 2.5rem;
+  z-index: 10000000;
 `
