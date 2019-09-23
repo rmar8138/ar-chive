b5abb28b88c06e94dc422fe3dd5c9f7b436354d1
rmar8138 <ragan.martinez@live.com>
2019-09-23 11:09:35 +1000

    Try fix styling issue when mobile site is initially loaded

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react"
 import DesktopHomePage from "../components/desktopHomePage"
 import MobileHomePage from "../components/mobileHomePage"
-import { isBrowser } from "react-device-detect"
+import { isBrowser, isMobile } from "react-device-detect"
 
 class Home extends Component {
   render() {
-    return isBrowser ? <DesktopHomePage /> : <MobileHomePage />
+    // return isBrowser ? <DesktopHomePage /> : <MobileHomePage />
+    return isMobile ? <MobileHomePage /> : <DesktopHomePage />
   }
 }
 
