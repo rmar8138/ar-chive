881780cdbb72f5a8af91d7dca8f35f7757f6805f
rmar8138 <ragan.martinez@live.com>
2019-10-09 15:32:13 +1100

    Try user agent package for styling issue

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react';
+import { UserAgentProvider } from '@quentin-sommer/react-useragent'
+
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <UserAgentProvider ua={window.navigator.userAgent}>{element}</UserAgentProvider>
+  );
+}
