f406ba4b2505d0ffb7069c107da0e672be23206e
rmar8138 <ragan.martinez@live.com>
2019-10-09 17:02:43 +1100

    Add UserAgent module to all pages

--- src/pages/montel-blac.js
+++ src/pages/montel-blac.js
@@ -1,87 +1,91 @@
 import React, { Fragment } from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import { UserAgent } from '@quentin-sommer/react-useragent'
 import ProjectPageLayout from "../components/projectPageLayout"
 import MobileProjectPageLayout from "../components/mobileProjectPageLayout"
-import { isBrowser } from "react-device-detect"
 
 const vid1 = require(`../assets/videos/montel-blac/1.mp4`)
 const vid2 = require(`../assets/videos/montel-blac/2.mp4`)
 
-const MontelBlac = ({ data }) =>
-  isBrowser ? (
-    <ProjectPageLayout heading="02/Montel Blac">
-      <Fragment>
-        <Img
-          fluid={data.img1.childImageSharp.fluid}
-          alt="Montel Blac"
-          style={{ maxWidth: "50%", minWidth: "50rem" }}
-        />
-      </Fragment>
-      <Fragment>
-        <p>
-          Montel Blac is a beatmaker and producer from Sydney. ar-chive worked
-          with Montel to create the identity of his self titled EP. Our goal was
-          to succinctly showcase both the sound and visual design in a way that
-          heightens Montel’s unearthly sound and off-kilter production style.
-        </p>
-      </Fragment>
-      <Fragment>
-        <figure>
-          <video loop muted controls="controls">
-            <source src={vid1} type="video/mp4" />
-          </video>
-        </figure>
-      </Fragment>
-      <Fragment>
-        <figure>
-          <video loop muted controls="controls">
-            <source src={vid2} type="video/mp4" />
-          </video>
-        </figure>
-      </Fragment>
-    </ProjectPageLayout>
-  ) : (
-    <MobileProjectPageLayout heading="02/Montel Blac">
-      <Fragment>
-        <Img
-          fluid={data.img1.childImageSharp.fluid}
-          alt="Montel Blac"
-          style={{ maxWidth: "50%", minWidth: "30rem" }}
-        />
-      </Fragment>
-      <Fragment>
-        <p>
-          Montel Blac is a beatmaker and producer from Sydney. ar-chive worked
-          with Montel to create the identity of his self titled EP. Our goal was
-          to succinctly showcase both the sound and visual design in a way that
-          heightens Montel’s unearthly sound and off-kilter production style.
-        </p>
-      </Fragment>
-      <Fragment>
-        <figure>
-          <video autoPlay loop muted>
-            <source
-              src={vid1}
-              type="video/mp4"
-              style={{ maxWidth: "50%", minWidth: "20rem" }}
-            />
-          </video>
-        </figure>
-      </Fragment>
-      <Fragment>
-        <figure>
-          <video autoPlay loop muted>
-            <source
-              src={vid2}
-              type="video/mp4"
-              style={{ maxWidth: "50%", minWidth: "20rem" }}
-            />
-          </video>
-        </figure>
-      </Fragment>
-    </MobileProjectPageLayout>
-  )
+const MontelBlac = ({ data }) => (
+  <Fragment>
+    <UserAgent computer>
+      <ProjectPageLayout heading="02/Montel Blac">
+        <Fragment>
+          <Img
+            fluid={data.img1.childImageSharp.fluid}
+            alt="Montel Blac"
+            style={{ maxWidth: "50%", minWidth: "50rem" }}
+          />
+        </Fragment>
+        <Fragment>
+          <p>
+            Montel Blac is a beatmaker and producer from Sydney. ar-chive worked
+            with Montel to create the identity of his self titled EP. Our goal was
+            to succinctly showcase both the sound and visual design in a way that
+            heightens Montel’s unearthly sound and off-kilter production style.
+          </p>
+        </Fragment>
+        <Fragment>
+          <figure>
+            <video loop muted controls="controls">
+              <source src={vid1} type="video/mp4" />
+            </video>
+          </figure>
+        </Fragment>
+        <Fragment>
+          <figure>
+            <video loop muted controls="controls">
+              <source src={vid2} type="video/mp4" />
+            </video>
+          </figure>
+        </Fragment>
+      </ProjectPageLayout>
+    </UserAgent>
+    <UserAgent mobile>
+      <MobileProjectPageLayout heading="02/Montel Blac">
+        <Fragment>
+          <Img
+            fluid={data.img1.childImageSharp.fluid}
+            alt="Montel Blac"
+            style={{ maxWidth: "50%", minWidth: "30rem" }}
+          />
+        </Fragment>
+        <Fragment>
+          <p>
+            Montel Blac is a beatmaker and producer from Sydney. ar-chive worked
+            with Montel to create the identity of his self titled EP. Our goal was
+            to succinctly showcase both the sound and visual design in a way that
+            heightens Montel’s unearthly sound and off-kilter production style.
+          </p>
+        </Fragment>
+        <Fragment>
+          <figure>
+            <video autoPlay loop muted>
+              <source
+                src={vid1}
+                type="video/mp4"
+                style={{ maxWidth: "50%", minWidth: "20rem" }}
+              />
+            </video>
+          </figure>
+        </Fragment>
+        <Fragment>
+          <figure>
+            <video autoPlay loop muted>
+              <source
+                src={vid2}
+                type="video/mp4"
+                style={{ maxWidth: "50%", minWidth: "20rem" }}
+              />
+            </video>
+          </figure>
+        </Fragment>
+      </MobileProjectPageLayout>
+    </UserAgent>
+  </Fragment>
+)
 
 export default MontelBlac
 
--- src/pages/other-worlds.js
+++ src/pages/other-worlds.js
@@ -1,257 +1,261 @@
 import React, { Fragment } from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import { UserAgent } from '@quentin-sommer/react-useragent'
 import ProjectPageLayout from "../components/projectPageLayout"
 import MobileProjectPageLayout from "../components/mobileProjectPageLayout"
-import { isBrowser } from "react-device-detect"
 
-const OtherWorlds = ({ data }) =>
-  isBrowser ? (
-    <ProjectPageLayout heading="10/Other Worlds">
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[4].node.childImageSharp.fluid}
-          alt="Other Worlds Front Cover"
-          style={{
-            marginLeft: "40%",
-            width: "35%",
-            minWidth: "25rem",
-            maxWidth: "40rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <p>
-          ‘Other Worlds’ is the debut album by Ragan Martinez. He uses ambient
-          and distorted sounds on loop, creating an ominous yet carthatic
-          meditative release. It is the soundtrack to a video game that doesn’t
-          exist. We art directed and designed the cover as well as individual
-          paintings for each track of the album.
-        </p>
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[9].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[6].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[2].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[10].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[0].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[8].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[3].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[7].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[5].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[1].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "70%",
-            minWidth: "40rem",
-            maxWidth: "80rem",
-          }}
-        />
-      </Fragment>
-    </ProjectPageLayout>
-  ) : (
-    <MobileProjectPageLayout heading="10/Other Worlds">
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[4].node.childImageSharp.fluid}
-          alt="Other Worlds Front Cover"
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <p>
-          ‘Other Worlds’ is the debut album by Ragan Martinez. He uses ambient
-          and distorted sounds on loop, creating an ominous yet carthatic
-          meditative release. It is the soundtrack to a video game that doesn’t
-          exist. We art directed and designed the cover as well as individual
-          paintings for each track of the album.
-        </p>
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[9].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[6].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[2].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[10].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[0].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[8].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[3].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[7].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[5].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-      <Fragment>
-        <Img
-          fluid={data.allFile.edges[1].node.childImageSharp.fluid}
-          alt=""
-          style={{
-            width: "100%",
-          }}
-        />
-      </Fragment>
-    </MobileProjectPageLayout>
-  )
+const OtherWorlds = ({ data }) => (
+  <Fragment>
+    <UserAgent computer>
+      <ProjectPageLayout heading="10/Other Worlds">
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[4].node.childImageSharp.fluid}
+            alt="Other Worlds Front Cover"
+            style={{
+              marginLeft: "40%",
+              width: "35%",
+              minWidth: "25rem",
+              maxWidth: "40rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <p>
+            ‘Other Worlds’ is the debut album by Ragan Martinez. He uses ambient
+            and distorted sounds on loop, creating an ominous yet carthatic
+            meditative release. It is the soundtrack to a video game that doesn’t
+            exist. We art directed and designed the cover as well as individual
+            paintings for each track of the album.
+          </p>
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[9].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[6].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[2].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[10].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[0].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[8].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[3].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[7].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[5].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[1].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "70%",
+              minWidth: "40rem",
+              maxWidth: "80rem",
+            }}
+          />
+        </Fragment>
+      </ProjectPageLayout>
+    </UserAgent>
+    <UserAgent mobile>
+      <MobileProjectPageLayout heading="10/Other Worlds">
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[4].node.childImageSharp.fluid}
+            alt="Other Worlds Front Cover"
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <p>
+            ‘Other Worlds’ is the debut album by Ragan Martinez. He uses ambient
+            and distorted sounds on loop, creating an ominous yet carthatic
+            meditative release. It is the soundtrack to a video game that doesn’t
+            exist. We art directed and designed the cover as well as individual
+            paintings for each track of the album.
+          </p>
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[9].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[6].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[2].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[10].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[0].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[8].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[3].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[7].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[5].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+        <Fragment>
+          <Img
+            fluid={data.allFile.edges[1].node.childImageSharp.fluid}
+            alt=""
+            style={{
+              width: "100%",
+            }}
+          />
+        </Fragment>
+      </MobileProjectPageLayout>
+    </UserAgent>
+  </Fragment>
+)
 
 export default OtherWorlds
 
