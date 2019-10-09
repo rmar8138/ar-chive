ceece291739f874208f7f00b118008c9a9181bed
rmar8138 <ragan.martinez@live.com>
2019-10-09 15:15:33 +1100

    Change isBrowser to BrowserView component

--- src/pages/a-love-below-live-vol-1.js
+++ src/pages/a-love-below-live-vol-1.js
@@ -3,98 +3,103 @@ import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import ProjectPageLayout from "../components/projectPageLayout"
 import MobileProjectPageLayout from "../components/mobileProjectPageLayout"
-import { isBrowser } from "react-device-detect"
+import { BrowserView, MobileView } from "react-device-detect"
 
-const ALoveBelow = ({ data }) =>
-  isBrowser ? (
-    <ProjectPageLayout heading="01/a Love Below: Live! Vol. 1">
-      <Fragment>
-        <figure>
-          <Img
-            fluid={data.img1.childImageSharp.fluid}
-            style={{ width: "40%", minWidth: "30rem", maxWidth: "40rem" }}
-          />
-        </figure>
-      </Fragment>
-      <Fragment>
-        <p>
-          Poster for a Love Below: Live! Volume 1, an afternoon of original
-          beats and painting created live from Coolmac Cafe in Kirribilli.
-          Featuring wa*sei, Angela Tam, Kase Avila, Montel Blac and Apollo 4000.
-        </p>
-      </Fragment>
-      <Fragment>
-        <figure>
-          <Img
-            fluid={data.img2.childImageSharp.fluid}
-            alt="a Love Below: Live! Vol. 1 - 2"
-            style={{ width: "60%", minWidth: "40rem", maxWidth: "70rem" }}
-          />
-        </figure>
-      </Fragment>
-      <Fragment>
-        <figure style={{ justifyContent: "flex-start", paddingLeft: "2.5rem" }}>
-          <Img
-            fluid={data.img3.childImageSharp.fluid}
-            alt="a Love Below: Live! Vol. 1 - 3"
-            style={{ width: "30%", minWidth: "15rem", maxWidth: "35rem" }}
-          />
-          <Img
-            fluid={data.img4.childImageSharp.fluid}
-            alt="a Love Below: Live! Vol. 1 - 4"
-            style={{ width: "30%", minWidth: "15rem", maxWidth: "35rem" }}
-          />
-        </figure>
-      </Fragment>
-    </ProjectPageLayout>
-  ) : (
-    <MobileProjectPageLayout heading="01/a Love Below: Live! Vol. 1">
-      <Fragment>
-        <figure>
-          <Img
-            fluid={data.img1.childImageSharp.fluid}
-            alt="a Love Below: Live! Vol. 1 - 1"
-            style={{ width: "80%" }}
-          />
-        </figure>
-      </Fragment>
-      <Fragment>
-        <p>
-          Poster for a Love Below: Live! Volume 1, an afternoon of original
-          beats and painting created live from Coolmac Cafe in Kirribilli.
-          Featuring wa*sei, Angela Tam, Kase Avila, Montel Blac and Apollo 4000.
-        </p>
-      </Fragment>
-      <Fragment>
-        <figure>
-          <Img
-            fluid={data.img2.childImageSharp.fluid}
-            alt="a Love Below: Live! Vol. 1 - 2"
-            style={{ width: "100%" }}
-          />
-          {/* <figcaption>Photos by Naylin Aung</figcaption> */}
-        </figure>
-      </Fragment>
-      <Fragment>
-        <figure>
-          <Img
-            fluid={data.img3.childImageSharp.fluid}
-            alt="a Love Below: Live! Vol. 1 - 3"
-            style={{ width: "80%" }}
-          />
-        </figure>
-      </Fragment>
-      <Fragment>
-        <figure>
-          <Img
-            fluid={data.img4.childImageSharp.fluid}
-            alt="a Love Below: Live! Vol. 1 - 4"
-            style={{ width: "80%", marginLeft: "auto" }}
-          />
-        </figure>
-      </Fragment>
-    </MobileProjectPageLayout>
-  )
+const ALoveBelow = ({ data }) => (
+  <Fragment>
+    <BrowserView>
+      <ProjectPageLayout heading="01/a Love Below: Live! Vol. 1">
+        <Fragment>
+          <figure>
+            <Img
+              fluid={data.img1.childImageSharp.fluid}
+              style={{ width: "40%", minWidth: "30rem", maxWidth: "40rem" }}
+            />
+          </figure>
+        </Fragment>
+        <Fragment>
+          <p>
+            Poster for a Love Below: Live! Volume 1, an afternoon of original
+            beats and painting created live from Coolmac Cafe in Kirribilli.
+            Featuring wa*sei, Angela Tam, Kase Avila, Montel Blac and Apollo 4000.
+          </p>
+        </Fragment>
+        <Fragment>
+          <figure>
+            <Img
+              fluid={data.img2.childImageSharp.fluid}
+              alt="a Love Below: Live! Vol. 1 - 2"
+              style={{ width: "60%", minWidth: "40rem", maxWidth: "70rem" }}
+            />
+          </figure>
+        </Fragment>
+        <Fragment>
+          <figure style={{ justifyContent: "flex-start", paddingLeft: "2.5rem" }}>
+            <Img
+              fluid={data.img3.childImageSharp.fluid}
+              alt="a Love Below: Live! Vol. 1 - 3"
+              style={{ width: "30%", minWidth: "15rem", maxWidth: "35rem" }}
+            />
+            <Img
+              fluid={data.img4.childImageSharp.fluid}
+              alt="a Love Below: Live! Vol. 1 - 4"
+              style={{ width: "30%", minWidth: "15rem", maxWidth: "35rem" }}
+            />
+          </figure>
+        </Fragment>
+      </ProjectPageLayout>
+    </BrowserView>
+    <MobileView>
+      <MobileProjectPageLayout heading="01/a Love Below: Live! Vol. 1">
+        <Fragment>
+          <figure>
+            <Img
+              fluid={data.img1.childImageSharp.fluid}
+              alt="a Love Below: Live! Vol. 1 - 1"
+              style={{ width: "80%" }}
+            />
+          </figure>
+        </Fragment>
+        <Fragment>
+          <p>
+            Poster for a Love Below: Live! Volume 1, an afternoon of original
+            beats and painting created live from Coolmac Cafe in Kirribilli.
+            Featuring wa*sei, Angela Tam, Kase Avila, Montel Blac and Apollo 4000.
+          </p>
+        </Fragment>
+        <Fragment>
+          <figure>
+            <Img
+              fluid={data.img2.childImageSharp.fluid}
+              alt="a Love Below: Live! Vol. 1 - 2"
+              style={{ width: "100%" }}
+            />
+            {/* <figcaption>Photos by Naylin Aung</figcaption> */}
+          </figure>
+        </Fragment>
+        <Fragment>
+          <figure>
+            <Img
+              fluid={data.img3.childImageSharp.fluid}
+              alt="a Love Below: Live! Vol. 1 - 3"
+              style={{ width: "80%" }}
+            />
+          </figure>
+        </Fragment>
+        <Fragment>
+          <figure>
+            <Img
+              fluid={data.img4.childImageSharp.fluid}
+              alt="a Love Below: Live! Vol. 1 - 4"
+              style={{ width: "80%", marginLeft: "auto" }}
+            />
+          </figure>
+        </Fragment>
+      </MobileProjectPageLayout>
+    </MobileView>
+  </Fragment>
+)
+  
 
 export default ALoveBelow
 
