015e04ee7c0e60a20b3302797b15f98643a6ceb6
rmar8138 <ragan.martinez@live.com>
2019-11-28 17:02:05 +1100

    Fix formatting for micra project

--- src/pages/micra.js
+++ src/pages/micra.js
@@ -12,63 +12,76 @@ const Micra = ({ data }) => (
         <Fragment>
           <Img
             fluid={data.img1.childImageSharp.fluid}
-            alt="Micra"
+            alt="Plastic single cover"
             style={{ width: "50%", minWidth: "30rem", maxWidth: "50rem" }}
           />
         </Fragment>
         <Fragment>
           <p>
-            Micra brings together multi-instrumentalist, Robbie Cain and
-            singer-guitarist, Ivana Kay. <br />
-            The Sydney based duo create a wistful, atmospheric sound with
-            floating melodies and vulnerable lyrical composition.
+            <span>
+              Micra brings together multi-instrumentalist, Robbie Cain and
+              singer-guitarist, Ivana Kay. The Sydney based duo create a
+              wistful, atmospheric sound with floating melodies and vulnerable
+              lyrical composition.
+            </span>
+            <span>
+              ar-chive art directed and photographed press images for Micra’s
+              release of their second single ‘Plastic’.
+            </span>
           </p>
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img2.childImageSharp.fluid}
-            alt="Micra"
+            alt="Press photos of Robbie and Ivana"
             style={{ width: "50%", minWidth: "30rem", maxWidth: "50rem" }}
           />
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img3.childImageSharp.fluid}
-            alt="Micra"
+            alt="Press photo of Micra"
             style={{ width: "40%", minWidth: "30rem", maxWidth: "40rem" }}
           />
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img4.childImageSharp.fluid}
-            alt="Micra"
+            alt="Press photo of Micra"
             style={{ width: "60%", minWidth: "40rem", maxWidth: "60rem" }}
           />
         </Fragment>
         <Fragment>
           <p>
-            We art directed and designed their covers for two debut singles,
-            ‘Child Grows Old’ and ‘Plastic’.
+            The first cover we worked on with Micra was their debut single,
+            ‘Child Grows Old’. The track invites listeners on a sonic journey
+            with a rhythm reminiscent of the nature of life’s serendipitous
+            timeline. At the time we were in New York City so it came naturally
+            to include elements of the city that influenced our perspective and
+            heightened the sense of distortion between expectations and one’s
+            experience.
           </p>
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img5.childImageSharp.fluid}
-            alt="Micra"
+            alt="Child Grows Old single cover"
             style={{ width: "60%", minWidth: "40rem", maxWidth: "60rem" }}
           />
         </Fragment>
         <Fragment>
-          <Img
-            fluid={data.img1.childImageSharp.fluid}
-            alt="Micra"
-            style={{ width: "60%", minWidth: "40rem", maxWidth: "60rem" }}
-          />
+          <p>
+            After listening to ‘Plastic’ we drew from the delicate vocals and
+            the transparency that comes with plastic material. It was essential
+            to experiment with the fragility of flowers and their petals and use
+            scanning as a visual element to further suggest the sense of
+            distortion that is found in the track.
+          </p>
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img10.childImageSharp.fluid}
-            alt="Micra"
+            alt="Process scans of vacuum sealed flowers"
             style={{ width: "80%", minWidth: "40rem", maxWidth: "80rem" }}
           />
         </Fragment>
@@ -79,80 +92,90 @@ const Micra = ({ data }) => (
         <Fragment>
           <Img
             fluid={data.img1.childImageSharp.fluid}
-            alt="Micra"
+            alt="Plastic single cover"
             style={{ width: "50%", minWidth: "30rem", maxWidth: "50rem" }}
           />
         </Fragment>
         <Fragment>
-          <p>
-            Micra brings together multi-instrumentalist, Robbie Cain and
-            singer-guitarist, Ivana Kay. The Sydney based duo create a wistful,
-            atmospheric sound with floating melodies and vulnerable lyrical
-            composition.
-          </p>
+          <div>
+            <p>
+              Micra brings together multi-instrumentalist, Robbie Cain and
+              singer-guitarist, Ivana Kay. The Sydney based duo create a
+              wistful, atmospheric sound with floating melodies and vulnerable
+              lyrical composition. ar-chive art directed and photographed press
+              images for Micra’s release of their second single ‘Plastic’.
+            </p>
+          </div>
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img11.childImageSharp.fluid}
-            alt="Micra"
+            alt="Press photo of Robbie"
             style={{ width: "45%", marginTop: "-15rem", marginRight: "1rem" }}
           />
           <Img
             fluid={data.img12.childImageSharp.fluid}
-            alt="Micra"
+            alt="Press photo of Ivana"
             style={{ width: "45%", marginTop: "15rem", marginLeft: "1rem" }}
           />
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img3.childImageSharp.fluid}
-            alt="Micra"
+            alt="Press photo of Micra"
             style={{ width: "90%", marginRight: "auto" }}
           />
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img4.childImageSharp.fluid}
-            alt="Micra"
+            alt="Press photo of Micra"
             style={{ width: "100%" }}
           />
         </Fragment>
         <Fragment>
           <p>
-            We art directed and designed their covers for two debut singles,
-            ‘Child Grows Old’ and ‘Plastic’.
+            The first cover we worked on with Micra was their debut single,
+            ‘Child Grows Old’. The track invites listeners on a sonic journey
+            with a rhythm reminiscent of the nature of life’s serendipitous
+            timeline. At the time we were in New York City so it came naturally
+            to include elements of the city that influenced our perspective and
+            heightened the sense of distortion between expectations and one’s
+            experience.
           </p>
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img5.childImageSharp.fluid}
-            alt="Micra"
+            alt="Child Grows Old single cover"
             style={{ width: "100%" }}
           />
         </Fragment>
         <Fragment>
-          <Img
-            fluid={data.img1.childImageSharp.fluid}
-            alt="Micra"
-            style={{ width: "100%" }}
-          />
+          <p>
+            After listening to ‘Plastic’ we drew from the delicate vocals and
+            the transparency that comes with plastic material. It was essential
+            to experiment with the fragility of flowers and their petals and use
+            scanning as a visual element to further suggest the sense of
+            distortion that is found in the track.
+          </p>
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img6.childImageSharp.fluid}
-            alt="Micra"
+            alt="Process scans of vacuum sealed flowers"
             style={{ width: "45%", marginTop: "-15rem", marginRight: "1rem" }}
           />
           <Img
             fluid={data.img7.childImageSharp.fluid}
-            alt="Micra"
+            alt="Process scans of vacuum sealed flowers"
             style={{ width: "45%", marginTop: "15rem", marginLeft: "1rem" }}
           />
         </Fragment>
         <Fragment>
           <Img
             fluid={data.img8.childImageSharp.fluid}
-            alt="Micra"
+            alt="Process scans of vacuum sealed flowers"
             style={{ width: "90%", marginRight: "auto" }}
           />
         </Fragment>
